6ce1dcbb82c3ecd932f905982d4f5da1ec081f7b
farhat <ibrahim.farhat@insa-rennes.fr>
2022-09-30 16:23:03 +0200

    [SAO] ARM functions initialization and adding new Edge filter functions

--- libovvc/arm/rcn_sao_neon_wrapper.c
+++ libovvc/arm/rcn_sao_neon_wrapper.c
@@ -42,25 +42,24 @@
 
 
 void ov_band_filter_neon(uint16_t *dst,const int16_t *src, int16_t sao_offset_val0, int16_t sao_offset_val1, int16_t sao_offset_val2, int16_t sao_offset_val3, uint8_t sao_left_class);
-// edge filter 10
-void ov_edge_filter_10_neon(uint16_t *dst,const int16_t *src, int a_stride, int b_stride,int16_t* sao_offset_val);
-void ov_edge_filter_7_10_neon1(uint16_t *dst,const int16_t *src, int a_stride, int b_stride,int16_t* sao_offset_val);
-void ov_edge_filter_7_10_neon2(uint16_t *dst,const int16_t *src, int a_stride, int b_stride,int16_t* sao_offset_val);
+void ov_sao_edge_filter_h_neon(uint16_t *dst,const int16_t *src, int a_stride, int b_stride,int16_t* sao_offset_val);
+void ov_sao_edge_filter_v_neon(uint16_t *dst,const int16_t *src, int a_stride, int b_stride,int16_t* sao_offset_val);
+void ov_sao_edge_filter_d_neon(uint16_t *dst,const int16_t *src, int a_stride, int b_stride,int16_t* sao_offset_val);
+void ov_sao_edge_filter_b_neon(uint16_t *dst,const int16_t *src, int a_stride, int b_stride,int16_t* sao_offset_val);
 
 static void
 sao_band_filter_0_10_neon(OVSample* _dst,
-                          OVSample* _src,
-                          ptrdiff_t _stride_dst,
-                          ptrdiff_t _stride_src,
-                          struct SAOParamsCtu* sao,
-                          int width,
-                          int height,
-                          int c_idx){
-
+                         OVSample* _src,
+                         ptrdiff_t _stride_dst,
+                         ptrdiff_t _stride_src,
+                         int width,
+                         int height,
+                         int8_t offset_val[],
+                         uint8_t band_pos)
+{
   int y, x;
-  //int shift = 10 - 5;
-  int16_t* sao_offset_val = sao->offset_val[c_idx];
-  uint8_t sao_left_class = sao->band_position[c_idx];
+  int shift = 10 - 5;
+
   uint16_t* dst = (uint16_t*)_dst;
   uint16_t* src = (uint16_t*)_src;
   ptrdiff_t stride_dst = _stride_dst;
@@ -68,94 +67,102 @@ sao_band_filter_0_10_neon(OVSample* _dst,
 
   for (y = 0; y < height; y++) {
     for (x = 0; x < width; x += 8) {
-      ov_band_filter_neon(dst, src, sao_offset_val[0], sao_offset_val[1], sao_offset_val[2], sao_offset_val[3], sao_left_class);
+
     }
     dst += stride_dst;
     src += stride_src;
   }
+}
 
 
+static void
+sao_edge_filter_v_neon(OVSample *dst, OVSample *src_row, OVSample *src_col,
+                      ptrdiff_t stride_dst, ptrdiff_t stride_src,
+                      int width, int height,
+                      int8_t offset_val[],
+                      uint8_t eo_dir)
+{
+  int x, y;
+
+  for (x = 0; x < width; x += 8) {
+    OVSample *src = dst;
+
+    for (y = 0; y < height; y++) {
+
+
+
+      src += stride_dst;
+    }
+    dst += 8;
+  }
 }
 
 static void
-sao_edge_filter_10_neon(OVSample* _dst,
-                        OVSample* _src,
-                        ptrdiff_t _stride_dst,
-                        ptrdiff_t _stride_src,
-                        SAOParamsCtu* sao,
-                        int width,
-                        int height,
-                        int c_idx){
-
+sao_edge_filter_d_neon(OVSample *dst, OVSample *src_row, OVSample *src_col,
+                      ptrdiff_t stride_dst, ptrdiff_t stride_src,
+                      int width, int height,
+                      int8_t offset_val[],
+                      uint8_t eo_dir)
+{
   int x, y;
-  int16_t* sao_offset_val = sao->offset_val[c_idx];
-  int eo = sao->eo_class[c_idx];
-  const int8_t pos[4][2][2] = {
-    { { -1, 0 }, { 1, 0 } },
-    { { 0, -1 }, { 0, 1 } },
-    { { -1, -1 }, { 1, 1 } },
-    { { 1, -1 }, { -1, 1 } },
-  };
-  uint16_t* dst = (uint16_t*)_dst;
-  uint16_t* src = (uint16_t*)_src;
-  ptrdiff_t stride_dst = _stride_dst;
-  ptrdiff_t stride_src = _stride_src;
 
-  int a_stride = pos[eo][0][0] + pos[eo][0][1] * stride_src;
-  int b_stride = pos[eo][1][0] + pos[eo][1][1] * stride_src;
+
+  for (x = 0; x < width; x += 8) {
+    OVSample *src = dst;
+
+    for (y = 0; y < height; y++) {
+
+      src += stride_dst;
+    }
+  }
+}
+
+static void
+sao_edge_filter_b_neon(OVSample *dst, OVSample *src_row, OVSample *src_col,
+                      ptrdiff_t stride_dst, ptrdiff_t stride_src,
+                      int width, int height,
+                      int8_t offset_val[],
+                      uint8_t eo_dir)
+{
+  int x, y;
+
+  for (x = 0; x < width; x += 8) {
+    OVSample *src = dst;
+
+    for (y = 0; y < height; y++) {
+
+
+
+      src += stride_dst;
+    }
+  }
+}
+
+
+static void
+sao_edge_filter_h_neon(OVSample *dst, OVSample *src_row, OVSample *src_col,
+                      ptrdiff_t stride_dst, ptrdiff_t stride_src,
+                      int width, int height,
+                      int8_t offset_val[],
+                      uint8_t eo_dir)
+{
+  int x, y;
+  OVSample *src = dst;
+
 
   for (y = 0; y < height; y++) {
+
     for (x = 0; x < width; x += 8) {
-      ov_edge_filter_10_neon(dst+x, src+x,(x+a_stride)>>1, (x+b_stride)>>1, sao_offset_val);
+
     }
-    src += stride_src;
+    src += stride_dst;
     dst += stride_dst;
   }
 }
-
-
-
-static void
-sao_edge_filter_7_10_neon(OVSample* _dst,
-                          OVSample* _src,
-                          ptrdiff_t _stride_dst,
-                          ptrdiff_t _stride_src,
-                          SAOParamsCtu* sao,
-                          int width,
-                          int height,
-                          int c_idx){
-  int x, y;
-  int16_t* sao_offset_val = sao->offset_val[c_idx];
-  int eo = sao->eo_class[c_idx];
-  const int8_t pos[4][2][2] = {
-    { { -1, 0 }, { 1, 0 } },
-    { { 0, -1 }, { 0, 1 } },
-    { { -1, -1 }, { 1, 1 } },
-    { { 1, -1 }, { -1, 1 } },
-  };
-  uint16_t* dst = (uint16_t*)_dst;
-  uint16_t* src = (uint16_t*)_src;
-  ptrdiff_t stride_dst = _stride_dst;
-  ptrdiff_t stride_src = _stride_src;
-
-  int a_stride = pos[eo][0][0] + pos[eo][0][1] * stride_src;
-  int b_stride = pos[eo][1][0] + pos[eo][1][1] * stride_src;
-
-
-  for (y = 0; y < height; y++) {
-    for (x = 0; x < width - width%8; x += 8) {
-      ov_edge_filter_10_neon1(dst+x, src+x,(x+a_stride)>>1, (x+b_stride)>>1, sao_offset_val);
-    }
-
-    ov_edge_filter_7_10_neon2(dst+x, src+x,(x+a_stride)>>1, (x+b_stride)>>1, sao_offset_val);
-    src += stride_src;
-    dst += stride_dst;
-  }
-}
-
-
 void rcn_init_sao_functions_neon(struct RCNFunctions *const rcn_funcs){
   rcn_funcs->sao.band= &sao_band_filter_0_10_neon;
-  rcn_funcs->sao.edge[0]= &sao_edge_filter_7_10_neon;
-  rcn_funcs->sao.edge[1]= &sao_edge_filter_10_neon;
+  rcn_funcs->sao.edge2[0]= &sao_edge_filter_h_neon;
+  rcn_funcs->sao.edge2[1]= &sao_edge_filter_v_neon;
+  rcn_funcs->sao.edge2[2]= &sao_edge_filter_d_neon;
+  rcn_funcs->sao.edge2[3]= &sao_edge_filter_b_neon;
 }
